b431e47909373ac637918bddb115b26b86c22725
S.F.A.M. Brouwer <Blueation@gmail.com>
2014-10-05 13:35:59 +0200

    forking over data

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace PizzaCourriers
     partial class Program
     {
         public static Random random = new Random();
-        public static Stopwatch stopwatch = new Stopwatch();
+        //public static Stopwatch stopwatch = new Stopwatch();
 
         public static List<Node> nodelist = new List<Node>();
         public static Bezorger[] bezorgers = new Bezorger[1];
@@ -19,6 +19,8 @@ namespace PizzaCourriers
         public static int CurrentCost = 0;
         public static int BestSolutionCost = 0;
         public static string BestSolutionOutput;
+        public static int BestestSolutionCost = 100000;
+        public static string BestestSolutionOutput;
 
         public static double cooldown;
         public static double temperature;
@@ -29,41 +31,61 @@ namespace PizzaCourriers
         public static int opt2chance = 50;
         public static int opt2halfchance = 50;
 
+        public static int RunN = 0;
+        public static int MaxRun = 500;
+
         static void Main(string[] args)
         {
-            //initialize uninitialized data, no clue wether these values are correct or not.
-            cooldown = 0.98;
-            temperature = 11.0;
-            limit = 1.0;
-            changepercooldown = 800;
-            imax = 1000000;
-
-            //load map into nodelist, restaurant into resX+resY
-            InputFromFile("GeoSquare1.txt");
-
-            //initialize map-dependant data
-            for (int num = 0; num < bezorgers.Length; num++)
-                bezorgers[num] = new Bezorger(num);
-
-            //create initial solution, optimality is not important, at all at this point
-            for (int i = 0; i < nodelist.Count; i++)
+            for (; RunN < 6000 && BestestSolutionCost > 130 ; RunN++)
             {
-                bezorgers[i % bezorgers.Length].AddNodetoEnd(nodelist[i]);
+                nodelist = new List<Node>();
+                bezorgers = new Bezorger[1];
+
+
+
+                //initialize uninitialized data, no clue wether these values are correct or not.
+                cooldown = 0.98;
+                temperature = 10.0;
+                limit = 1.0;
+                changepercooldown = 800;
+                imax = 1000000;
+
+                //load map into nodelist, restaurant into resX+resY
+                InputFromFile("GeoSquare1.txt");
+
+                //initialize map-dependant data
+                for (int num = 0; num < bezorgers.Length; num++)
+                    bezorgers[num] = new Bezorger(num);
+
+                //create initial solution, optimality is not important, at all at this point
+                for (int i = 0; i < nodelist.Count; i++)
+                {
+                    bezorgers[i % bezorgers.Length].AddNodetoEnd(nodelist[i]);
+                }
+                foreach (Bezorger B in bezorgers)
+                    BestSolutionCost += B.routeLength;
+                CurrentCost = BestSolutionCost;
+                BestSolutionOutput = StringSolution();
+                if (RunN == 0)
+                    BestestSolutionCost = CurrentCost;
+
+                //Console.WriteLine(BestSolutionCost);
+                //Console.WriteLine(BestSolutionOutput);
+
+                //optimalize
+                SimulatedAnealing();
+
+                if (BestSolutionCost < BestestSolutionCost)
+                {
+                    BestestSolutionCost = BestSolutionCost;
+                    BestestSolutionOutput = BestSolutionOutput;
+                }
             }
-            foreach (Bezorger B in bezorgers)
-                BestSolutionCost += B.routeLength;
-            CurrentCost = BestSolutionCost;
-            BestSolutionOutput = StringSolution();
-            Console.WriteLine(BestSolutionCost);
-            Console.WriteLine(BestSolutionOutput);
-            
-            //optimalize
-            SimulatedAnealing();
 
             //output
-            BestSolutionOutput = StringSolution();
-            Console.WriteLine(BestSolutionCost);
-            Console.WriteLine(BestSolutionOutput);
+            
+            Console.WriteLine(BestestSolutionCost);
+            Console.WriteLine(BestestSolutionOutput);
             foreach (Bezorger b in bezorgers)
                 Console.WriteLine(b.GetLength());
             Console.ReadLine();
